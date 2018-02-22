2935b71166f564c026e8ffe57e5cf4cfc37368a9
root <root@Amy.localdomain>
2018-02-22 16:41:23 +0000

    Fixing indendation error in the view.py file

---
+++ app/view.py
@@ -0,0 +1,14 @@
+"""Main module."""
+from flask import render_template
+from app import app
+import systeminfo.main
+
+@app.route('/')
+def index():
+        returnDict = {}
+        returnDict['user'] = 'Amy'
+        returnDict['title'] = 'Flask Application'
+		returnDict['info'] = systeminfo.main.main()
+        return render_template("index.html", **returnDict)
+
+
