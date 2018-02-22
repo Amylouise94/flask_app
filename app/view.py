a1336a417fcd181c5dd8b425ce261c8ef15416ff
root <root@Amy.localdomain>
2018-02-22 10:53:07 +0000

    Updated app.py

--- app/view.py
+++ app/view.py
@@ -1,13 +1,14 @@
 """Main module."""
 from flask import render_template
 from app import app
-import sysinfo
+import systeminfo.main
 
 @app.route('/')
 def index():
         returnDict = {}
         returnDict['user'] = 'Amy'
         returnDict['title'] = 'Flask Application'
-	returnDict['info'] = sysinfo.main()
+	returnDict['info'] = systeminfo.main.main()
         return render_template("index.html", **returnDict)
+
 
